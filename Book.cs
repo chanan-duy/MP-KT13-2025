9baf719e8e85af6bb3a40f36358759e2ac495824
chanan-duy <chanan.duy@proton.me>
2025-09-07 11:46:36 +0500

    Init Book

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿namespace MP_KT13_2025;
+
+public class Book
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Book(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} with {Price} price";
+    }
+}
