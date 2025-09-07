3cc39f486649bbc09f636ff6e8c28835b3bfa300
chanan-duy <chanan.duy@proton.me>
2025-09-07 11:37:38 +0500

    Test Swap

--- Program.cs
+++ Program.cs
@@ -22,9 +22,30 @@ public static class MainLogic
         Console.WriteLine(Helpers.Max(d1, d2));
     }
 
+
+    private static void TestSwap()
+    {
+        var i1 = 123;
+        var i2 = 321;
+        Helpers.Swap(ref i1, ref i2);
+        Console.WriteLine($"i1 = {i1} ; i2 = {i2}");
+
+        var s1 = "123";
+        var s2 = "1234";
+        Helpers.Swap(ref s1, ref s2);
+        Console.WriteLine($"s1 = {s1} ; s2 = {s2}");
+
+        var person1 = new Person("p1");
+        var person2 = new Person("p2");
+        Helpers.Swap(ref person1, ref person2);
+        Console.WriteLine($"p1 = {person1} ; p2 = {person2}");
+    }
+
     private static void TestHelpers()
     {
         TestMax();
+        PrintNewLine();
+        TestSwap();
         PrintNewLine();
     }
 
