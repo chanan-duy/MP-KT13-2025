4fd4a49e2afd56268449dc8876bee79edd5bc78b
chanan-duy <chanan.duy@proton.me>
2025-09-07 11:33:11 +0500

    Init Helpers with Max impl

---
+++ Helpers.cs
@@ -0,0 +1,9 @@
+﻿namespace MP_KT13_2025;
+
+public static class Helpers
+{
+    public static T Max<T>(T x, T y) where T : IComparable<T>
+    {
+        return x.CompareTo(y) >= 0 ? x : y;
+    }
+}
